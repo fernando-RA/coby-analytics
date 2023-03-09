9e3b8723248ef5fdaa373135360df52d7032954b
Fernando Aguilar <fraguilar@pm.me>
2023-03-09 13:12:18 -0300

    feat: adding initial reports data

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/MercadoBitcoin',
+        destination: '/MercadoBitcoin.html',
+      },
+      {
+        source: '/CryptoTitans',
+        destination: '/CryptoTitans.html',
+      },
+      {
+        source: '/KeepBored',
+        destination: '/KeepBored.html',
+      }
+    ]
+  },
   experimental: {
     appDir: true,
   },
